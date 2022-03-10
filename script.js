f6f3d3bf1a9c9bc050b14c91d465fe13e47e3d3e
mrkleiner <megaadrenaline1055@gmail.com>
2022-03-10 15:01:32 +0200

    final

--- script.js
+++ script.js
@@ -1,80 +1,110 @@
 window.allcards = [
 
 	{
-		'name': 'Robert Lewandowski (21.08.1988)',
+		'name': 'Robert Lewandowski',
 		'desc': 'Robert Lewandowski (Lewandowski) is a famous polish football player. He is the author of a rare, even incredible phenomenon in football called the penta-trick. He was not a pioneer in this movement, before him Dimitar Berbatov, Cristiano Ronaldo and Lionel Messi had achieved similar successes. Another thing is that Robert scored five goals in nine minutes, thereby setting a time record.',
-		'photo': 'robert_l'
+		'photo': 'robert_l',
+		'date': '(August 21, 1988)',
+		'cid': 'vFA'
 	},
 	{
-		'name': 'Jan Pawel II (May 18, 1920 - April 2, 2005)',
+		'name': 'Jan Paweł II',
 		'desc': 'John Paul II was the first Slav on the Holy See. His pontificate - one of the longest in the history of the papacy - helped to spread the ideals of tolerance, a return to universal values and the destruction of the "evil empire".',
-		'photo': 'jan_pawel'
+		'photo': 'jan_pawel',
+		'date': '(May 18, 1920 - April 2, 2005)',
+		'cid': 'igI'
 	},
 	{
 		'name': 'Maria Skłodowska-Curie',
 		'desc': 'Marie Curie - French scientist of Polish descent, known for his work in the field of radioactivity. To this day, she has been one of the most important women in science. She was the first woman to receive the Nobel Prize, an honor she and her husband, Pierre Curie, received. Recognition was awarded to the couple in the category of "Physics" for research on the phenomenon of radiation, discovered by Henri Becquerel.',
-		'photo': 'marie_curie'
+		'photo': 'marie_curie',
+		'date': '(November 7, 1867 - July 4, 1934)',
+		'cid': 'X2n'
 	},
 	{
 		'name': 'Fryderyk Franciszek Szopen',
 		'desc': 'He was a major representative of Polish music. He reinterpreted many genres: he revived the romantic prelude, created a piano ballad, poetized and dramatized dances - mazurka, polonaise, waltz. He turned the scherzo into an independent work. He enriched the harmony and the piano texture, and combined the classicism of form with melodic richness and imagination.',
-		'photo': 'fredrich_shopen'
+		'photo': 'fredrich_shopen',
+		'date': '(1810 - October 17, 1849)',
+		'cid': 'lFp'
 	},
 	// біографія
 	{
 		'name': 'Mikolai Kopernik',
 		'desc': 'He is primarily known as the creator of the heliocentric picture of the world (basically he stated that our planetary system looks the way it is). He mathematically substantiated the idea of ​​the motion of the Earth and other planets around the Sun, determined the sequence of planets, calculated their relative distance from the Sun, and so on.',
-		'photo': 'kopernik'
+		'photo': 'kopernik',
+		'date': '(February 19, 1473 - May 24, 1543)',
+		'cid': 'qcn'
 	},
 	{
 		'name': 'Adam Malysz',
 		'desc': 'Since 1996, Adam Malysz has won 38 World Cup victories, making his name at the world’s greatest ski jumping competitions: the World Championships four times, the World Cup four times and the Four Hills tournament in 2001.',
-		'photo': 'adam_mal'
+		'photo': 'adam_mal',
+		'date': '(December 3, 1977)',
+		'cid': 'I38'
 	},
 	{
 		'name': 'Andrzej Wajda',
 		'desc': 'He was a Polish theatre and film director, a classic of European auteur cinema. Wajda won wide acclaim for his film "Canal" (1957), which won several awards, including a special prize at the Cannes Film Festival in 1957.',
-		'photo': 'andrei_wajda'
+		'photo': 'andrei_wajda',
+		'date': '(March 6, 1926 - October 9, 2016)',
+		'cid': 'My0'
 	},
 	{
 		'name': 'Czesław Miłosz',
 		'desc': 'Czeslaw Milosz is a Polish-American poet, novelist, translator and diplomat. Considered one of the greatest poets of the twentieth century, he received the Nobel Prize for Literature in 1980.',
-		'photo': 'milosh'
+		'photo': 'milosh',
+		'date': '(June 30, 1911 - August 14, 2004)',
+		'cid': 'qQX'
 	},
 	{
-		'name': 'Zbigeniew Preisner',
-		'desc': 'Zbigniew Praisner is best known as the author of music for the films of the famous Polish director Krzysztof Kieslowski. In some of them Praisner\'s music is assigned to the fictional Dutch composer Van den Budenmaer.',
-		'photo': 'preisner'
+		'name': 'Zbigniew Preisner',
+		'desc': 'Zbigniew Preisner is best known as the author of music for the films of the famous Polish director Krzysztof Kieslowski. In some of them Praisner\'s music is assigned to the fictional Dutch composer Van den Budenmaer.',
+		'photo': 'preisner',
+		'date': '(May 20, 1955)',
+		'cid': 'qYT'
 	},
 	{
-		'name': 'Robert Mak Lowicz',
+		'name': 'Robert Witold Makłowicz',
 		'desc': 'Robert is a Polish journalist and historian, who is well-known for promoting polish TV culture and food, the descriptions he made became so popular that he was moved to the all-national weekly magazine published by Gazeta Wyborcza.',
-		'photo': 'robert'
+		'photo': 'robert',
+		'date': '(August 12, 1963)',
+		'cid': '8Rc'
 	},
 	{
 		'name': 'Olga Tokarczuk',
 		'desc': 'Olga is a writer and poetess, winner of some international awards and a Nobel Prize. Tokarczuk is one of the most critically acclaimed and most translated Polish writers',
-		'photo': 'olga'
+		'photo': 'olga',
+		'date': '(January 29, 1962)',
+		'cid': 'DIv'
 	},
 	{
 		'name': 'Agnieszka Holland',
 		'desc': 'Olga is a writer and poetess, winner of some international awards and a Nobel Prize. Tokarczuk is one of the most critically acclaimed and most translated Polish writers',
-		'photo': 'agnieszka'
+		'photo': 'agnieszka',
+		'date': '(November 28, 1948)',
+		'cid': '9fh'
 	},
 	{
 		'name': 'Joanna Jędrzejczyk',
 		'desc': 'She is a Polish mixed martial arts fighter, five-time world champion and four-time European amateur Thai boxing champion. Jędrzejczyk has competed in Thai boxing and kickboxing for more than 10 years, winning more than 60 fights',
-		'photo': 'Joanna'
+		'photo': 'Joanna',
+		'date': '(August 18, 1987)',
+		'cid': 'lc6'
 	},	
 	{
 		'name': 'Andrzej Sapkowski',
 		'desc': 'Andrzej Sapkowski is a famous Polish writer and publicist. Best known as the author of the books about the adventures of the witch Heralt of Rivia.',
-		'photo': 'sapkowski'
+		'photo': 'sapkowski',
+		'date': '(June 21, 1948)',
+		'cid': '9xC'
 	},
 	{
-		'name': 'Jan Jaromir Aleksium',
+		'name': 'Jan Jaromir Aleksiun',
 		'desc': 'Jan Jaromir was a renowned pedagogue of several generations of graduates of the Academy of Fine Arts in Wroclaw and in Warsaw. He was also a graphic artist, painter, illustrator, and a renowned author of posters.',
-		'photo': 'jan_jaromir'
+		'photo': 'jan_jaromir',
+		'date': '(February 2, 1940)',
+		'cid': 'MGN'
 	},
 	// {
 	// 	'name': 'Jerzy Wozniak',
@@ -84,17 +114,23 @@ window.allcards = [
 	{
 		'name': 'Ewa Szumanska',
 		'desc': 'She was an actress, writer, traveler, co-creator of the satirical show "Studio 202" on the Polish radio, famous in particular for her cabaret role of the Young Doctor in cabaret sketches "From Young Doctor\'s Diary". After the war, she settled in Wroclaw, where she got involved in shaping the city\'s culture.',
-		'photo': 'ewa'
+		'photo': 'ewa',
+		'date': '(May 20, 1921 - May 17, 2011)',
+		'cid': 'pN5'
 	},
 	{
-		'name': 'Marek petrusewicz',
+		'name': 'Marek Petrusewicz',
 		'desc': 'He was a double world record breaker for 100 meters in breaststroke, vice champion of Europe for 200 meters, the representative of Poland in swimming during XV Summer Olympic Games in Helsinki. He was one of the brightest stars of Polish sport',
-		'photo': 'marek'
+		'photo': 'marek',
+		'date': '(February 22, 1934 - October 3, 1992)',
+		'cid': 'FYN'
 	},
 	{
 		'name': 'Zdzisław Beksiński',
 		'desc': 'Zdzisław Beksiński was one of the most popular Polish artists of the 20th century, Zdzislaw Beksinski, almost never gave any titles to his works. Because of this, his post-apocalyptic canvases are perceived as a kind of world of horror, despair, or whatever else you can see in his paintings.',
-		'photo': 'polish_giger'
+		'photo': 'polish_giger',
+		'date': '(February 24, 1929 - February 21, 2005)',
+		'cid': 'xYO'
 	}
 ]
 
@@ -142,6 +178,108 @@ document.addEventListener('click', event => {
 });
 
 
+document.addEventListener('change', event => {
+    console.log('click_registered');
+    // pool
+    const pool = event.target.closest('#game_players input');
+    if (pool) {
+    	if (parseInt($(pool).attr('max')) < $(pool).val()){
+    		$(pool).val($(pool).attr('max'))
+    	}
+
+       	if (parseInt($(pool).attr('min')) > $(pool).val()){
+    		$(pool).val($(pool).attr('min'))
+    	}
+    }
+});
+
+
+
+
+
+window.mrk_ect_timer; // create container for tolltip timer
+window.mrk_ect_tooltip_timeout = 50; // tooltip timeout
+
+
+		document.addEventListener('mousemove'
+			, function(e){
+				var x = e.clientX;
+				var y = e.clientY
+				// window.cs_x = x + "px";
+				// window.cs_y = y + "px";
+				window.cs_x = x;
+				window.cs_y = y;
+			});
+
+
+document.addEventListener('mouseover', event => {
+    // ===========================
+    //           Tooltip
+    // ===========================
+	const cursor_over_tooltip_obj = event.target.closest('*[mrk_ect_tooltip]');
+	if (cursor_over_tooltip_obj)
+	{
+		mrk_ect_timer = setTimeout(function() {
+			console.log('delayed call');
+			console.log(event.clientX, event.clientY)
+
+			var etgt = cursor_over_tooltip_obj;
+
+			var target = cursor_over_tooltip_obj
+
+			var celemheight = $(target).height()
+			var celemwidth = $(target).width()
+
+		    // Get the bounding rectangle of target
+		    var rect = target.getBoundingClientRect();
+
+		    // Mouse position
+		    var x = etgt.clientX - rect.left;
+		    var y = etgt.clientY - rect.top;
+
+		    var createshift = rect.bottom
+
+			// $('.mrk_ect_tooltip_box').css({top: window.cs_y + 'px', left: (window.cs_x - 30) + 'px'});
+			$('.mrk_ect_tooltip_box').css({top: createshift + 'px', left: (window.cs_x - 30) + 'px'});
+			$('.mrk_ect_tooltip_box').removeClass('class_hidden');
+			$('.mrk_ect_tooltip_box').text($(cursor_over_tooltip_obj).attr('mrk_ect_tooltip'));
+		}, mrk_ect_tooltip_timeout);
+	}
+    // ===========================
+    //           Tooltip
+    // ===========================
+});
+
+document.addEventListener('mouseout', event => {
+    // ===========================
+    //           Tooltip
+    // ===========================
+	const cursor_over_tooltip_obj_leave_soon = event.target.closest('*[mrk_ect_tooltip]');
+	if (cursor_over_tooltip_obj_leave_soon)
+	{
+		clearTimeout(mrk_ect_timer);
+		$('.mrk_ect_tooltip_box').addClass('class_hidden');
+	}
+    // ===========================
+    //           Tooltip
+    // ===========================
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function docards()
 {
@@ -158,6 +296,7 @@ function docards()
 					<div class="table_card">
 						<div class="table_card_pic"><img draggable="false" src="assets/` + rnd_item['photo'] + `.png"></div>
 						<div class="table_card_name">` + rnd_item['name'] + `</div>
+						<div class="table_card_date">` + rnd_item['date'] + `</div>
 						<div class="table_card_desc">` + rnd_item['desc'] + `</div>
 					</div>
 				`)
@@ -179,6 +318,7 @@ function showpool()
 				<div class="table_card">
 					<div class="table_card_pic"><img draggable="false" src="assets/` + cr['photo'] + `.png"></div>
 					<div class="table_card_name">` + cr['name'] + `</div>
+					<div class="table_card_date">` + cr['date'] + `</div>
 					<div class="table_card_desc">` + cr['desc'] + `</div>
 				</div>
 			`)
